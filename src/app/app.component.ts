bc8df04b00be6fb148b2d9067eeef309ea56140d
Dany Paredes <danywalls4@gmail.com>
2024-03-31 16:53:36 +0200

    feat: 🎸 use ngOptimizeImage directive
    implement ngOptimizeImage directive
    
    BREAKING CHANGE: 🧨 N/A

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {ProductsService} from "./services/products.service";
+import {NgOptimizedImage} from "@angular/common";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  imports: [
+    NgOptimizedImage
+  ],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'products-offers';
+
+  $products = inject(ProductsService).$products;
+
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {toSignal} from "@angular/core/rxjs-interop";
+
+export type Product = {
+  description: string;
+  image: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  private API = 'https://fakestoreapi.com/products'
+  http = inject(HttpClient)
 
-  constructor() { }
+  $products = toSignal(this.http.get<Array<Product>>(this.API));
 }
